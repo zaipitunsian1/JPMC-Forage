7bee5bbb05ecd5c295032c2dba94964fc5078564
zaipitunsian1 <93256267+zaipitunsian1@users.noreply.github.com>
2022-12-14 13:11:05 +0530

    Update client3.py

--- 1. Interface with a stock price data feed/jpm_module_1/client3.py
+++ 1. Interface with a stock price data feed/jpm_module_1/client3.py
@@ -13,8 +13,6 @@ QUERY = "http://localhost:8080/query?id={}"
 N = 500
 
 def getDataPoint(quote):
-	""" Produce all of the needed values to generate a datapoint """
-	""" ------------- Update this function ------------- """
 	stock = quote['stock']
 	bid_price = float(quote['top_bid']['price'])
 	ask_price = float(quote['top_ask']['price'])
@@ -22,10 +20,6 @@ def getDataPoint(quote):
 	return stock, bid_price, ask_price, price
 
 def getRatio(price_a, price_b):
-  
-	""" Get ratio of price_a and price_b """
-	""" ------------- Update this function ------------- """
-	""" Also create some unit tests for this function in client_test.py """
 	if(price_b==0):
             return  price_a/ price_b
   
@@ -37,14 +31,10 @@ if __name__ == "__main__":
 	# Query the price once every N seconds.
 	for _ in range(N):
 		quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
-
-		""" ----------- Update to get the ratio --------------- """
 		prices = {}
-    
 		for quote in quotes:
 			stock, bid_price, ask_price, price = getDataPoint(quote)
 			prices[stock] = price
-
 			print ("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
 		print ("Ratio %s" % getRatio(prices['ABC'], prices['DEF']))
